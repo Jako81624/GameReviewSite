2fb4f7327b05dd28cb897cdbe1f0f6fe8875a22c
Mateusz Ulinski <post159510@gmail.com>
2015-12-12 14:01:55 +0000

    Active class and layout standardisation

--- readme.md
+++ readme.md
@@ -1,2 +1,9 @@
 # Game Review Site
 A git repository of HTML/CSS stuff for the eventual formation of an open games reviewing site
+
+Current status:
+- Homepage active
+- Laravel setup
+- Placeholder All Games Page Active
+- Smooth Scrolling/Carousel/Navbar features active
+- Rudimentary Mobile Support
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,27 +9,9 @@
 </head>
 <body data-spy="scroll" data-target=".navbar" data-offset="50">
 <nav class="navbar navbar-inverse navbar-fixed-top top-test">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand page-scroll" href="#featured">OCGR</a>
-        </div>
-        <div>
-            <div class="collapse navbar-collapse top-test-enable" id="myNavbar">
-                <ul class="nav navbar-nav">
-                    <li><a class="page-scroll" href="#featured">Featured</a></li>
-                    <li><a class="page-scroll" href="#latest">Latest</a></li>
-                    <li><a href="/allgames">All Games</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
+    @yield('navbar')
 </nav>
-@yield('content')
+    @yield('content')
 <div class="navbar-fixed-bottom">
     <p>Open-source and Unlicensed</p>
 </div>
