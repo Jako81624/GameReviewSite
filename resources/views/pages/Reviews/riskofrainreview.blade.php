1370c1c63884be53fd8f2a7b22df10077ea8f0b8
Jako81624 <jacobwlane@gmail.com>
2016-01-21 18:48:10 +0000

    Major Changes 2.0
    Made sure not to screw over @Posttwo too hard.  Kappa ;)

---
+++ resources/views/pages/Reviews/riskofrainreview.blade.php
@@ -0,0 +1,91 @@
+@extends('layouts.review')
+@section('head')
+<link href="{{ elixir('css/review.css') }}" rel="stylesheet" type="text/css">
+<style>
+.jumbotron {
+	height: 400px;
+	background-image: url('/Carousel/RiskOfRain/2.jpg');
+	background-size: cover;
+}
+</style>
+@endsection
+
+@section('content')
+<div class="jumbotron">
+	<div class="container">
+		<h1 id="Top">Risk Of Rain Review</h1>
+	</div>
+</div>
+
+<div id="review">
+<div class="container">
+	<p>What do you get when you mix Super Mario Bros with guns, explosions, giant monsters and tons of items?  Well it looks like we’ve got a Risk of Rain.</p>
+	<h2 id="Gameplay">Gameplay</h2>
+	<p>Risk of Rain is a procedurally generated rogue-like platformer.  Players pick a character from a set of thirteen characters (only one is unlocked at the beginning).  
+	Each of these characters attacks and defends themselves from the planetary wildlife with a varying set of four moves.   These four moves shape the playstyle of each 
+	character which help adds to the replayability and keeps things fresh no two characters are play exactly alike.  Scattered around each map are crates which contain 
+	items to help your character in their journey.  These items add effects such as increased attack speed, faster healing, one revive, all of your attacks explode or even 
+	just extra money for each kill.  These items are great and none are truly useless.  This is important as they are the bread and butter of Risk of Rain.  However, the most 
+	important part of Risk of Rain is the difficulty.   As time goes on, the difficulty gets harder faster.  This is important as it causes you to try and manage whether it 
+	is better to go to a level when it is easier early on or dawdle and get items to become even stronger.   Though the difficulty does eventually cap, it is over 45 minutes 
+	into a single playthrough and is unrelentingly difficult.  What gives this game its flair and life is that as the game gets punishingly difficult you begin to feel somewhat 
+	like a god.  With upwards of 100 items you become death incarnate.  However, Risk of Rain has no qualms about reminding you that you are infact mortal, with enemies 
+	swatting you from full health to no health with ease.  If you love a challenge, Risk of Rain is for you.</p>
+	<h2 id="Story">Story</h2>
+	<p>Risk of Rain's story is one of survival.  After your cargo spaceship is infiltrated by a mysterious figure, it crash lands on a mysterious planet you 
+	have to fight your way through hordes of enemies to make it back to your ship for...reasons?  I’m not really sure why you’re trying to make it back to your 
+	ship to be honest, the story was pretty lackluster, however it was never the main selling point of the game.</p>
+	<h2 id="Graphics">Graphics, Settings and Performance</h2>
+	<h4>Graphics</h4>
+	<p>I am not usually a fan of 8-bit pixel graphics but Risk of Rain won me over.  The graphics wonderfully define the game and make everything from the characters, 
+	monsters and particle effects look great.  The graphics are not at all demanding.  I was able to run a solid 60 FPS on my rig with VSync off.  The settings are comprehensive 
+	with the ability to turn down the graphics if need be as well as the particle effects as well as sound and the zoom level.  However, if you’ve updated your computer within 
+	the last 12 years, you should be fine.</p>
+	<h2 id="Conclusion">Conclusion</h2>
+	<p>Starting Risk of Rain is like jumping into a puddle and finding out its actually an ocean.  The amount of content, challenge and enjoyment in this seemingly simple 
+	game is mind boggling.  The difficulty and fun are at a near perfect level with you never really being able to blame the game for being unfair, just yourself for being stupid.  
+	Coupled with an AMAZING soundtrack, Risk of Rain is a must have in my opinon. Pick it up on Steam right now for $2.49 and check out Hopoo Games’ new game, Deadbolt.</p>
+	<p>My final score is a 9/10.  This game is truly amazing.</p>
+	<ul>
+	<li>Game: Risk Of Rain</li>
+	<li>Out: Now</li>
+	<li>Developer: Hopoo Games</li>
+	<li>Publisher: Chucklefish Games</li>
+	<li>Platform reviewed on: PC</li>
+	</ul>
+
+	<h2>--------------------------------------</h2>
+	<h2>Rating: 9/10 - Near Perfection</h2>
+	<h2>--------------------------------------</h2>
+
+	<h2 id="Screenshots">Screenshots</h2>
+
+	<div class="row">
+		<div class="col-md-4">
+			<div class="thumbnail">
+			<img src="/Carousel/RiskOfRain/1.jpg">
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="thumbnail">
+			<img src="/Carousel/RiskOfRain/2.jpg">
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="thumbnail">
+			<img src="/Carousel/RiskOfRain/3.jpg">
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-4">
+			<div class="thumbnail">
+			<img src="/Carousel/RiskOfRain/4.jpg">
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+
+@endsection
