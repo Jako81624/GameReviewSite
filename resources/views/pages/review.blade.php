f7c143a84343f50a5812a063899dcc456ff035da
Mateusz Ulinski <post159510@gmail.com>
2016-01-22 21:22:31 +0000

    change admin sidebar and fix navbar

--- resources/views/pages/review.blade.php
+++ resources/views/pages/review.blade.php
@@ -25,7 +25,16 @@
                 <div class="col-md-4">
                     Article By: {{$user['name']}}<br/>
                     @allows('article.admin')
-                    <button>Administrate</button>
+					<h3>Administration</h3>
+					<table class="table table-condensed table-hover" style="width: 60%">
+						<tr><th>Property</th><th>Value</th></tr>
+						<tr><td>ID</td><td>{{$id}}</td></tr>
+						<tr><td>SLUG</td><td>{{$slug}}</td></tr>
+						<tr><td>STORE</td><td>{{$store or 'N/A'}}</td></tr>
+						<tr><td>APPID</td><td>{{$sotre_app_id or 'N/A'}}</td></tr>
+						<tr><td>PUBLIC</td><td>{{$published}}</td></tr>
+						<tr><td>CREATED</td><td>{{$created_at}}</td></tr>
+					</table>
                     @endallows
                 </div>
             </div>
