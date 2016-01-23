9ee0356a3f69fbbf8ea5e0869048f864c9bc79ac
Mateusz Ulinski <post159510@gmail.com>
2016-01-23 16:35:24 +0000

    Make thrown exceptions in json for ajax requests

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,37 @@ class Handler extends ExceptionHandler
         if ($e instanceof ModelNotFoundException) {
             $e = new NotFoundHttpException($e->getMessage(), $e);
         }
+		if ($request->wantsJson())
+		{
+			// Define the response
+			$response = [
+				'errors' => 'Sorry, something went wrong.'
+			];
+
+			// If the app is in debug mode
+			if (config('app.debug'))
+			{
+				// Add the exception class name, message and stack trace to response
+				$response['exception'] = get_class($e); // Reflection might be better here
+				$response['message'] = $e->getMessage();
+				$response['trace'] = $e->getTrace();
+			}
+
+			// Default response of 400
+			$status = 400;
+
+			// If this exception is an instance of HttpException
+			if ($this->isHttpException($e))
+			{
+				// Grab the HTTP status code from the Exception
+				$status = $e->getStatusCode();
+			}
+
+			// Return a JSON response with the response array and status code
+			return response()->json($response, $status);
+		}
+
+		// Default to the parent class' implementation of handler
         return parent::render($request, $e);
     }
 }
