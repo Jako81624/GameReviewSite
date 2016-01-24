dae3687cf77e21d66591556ee8c40e3ceece6fff
Posttwo <posttwo@users.noreply.github.com>
2016-01-24 02:29:39 +0000

    its 6am

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -54,7 +54,7 @@ class ArticleController extends Controller
     {
         if($article->published == false)
         {
-            if(!Permissions::check('article.view-unpublished') OR $article->user_id != Auth::user()->id)
+            if(!Permissions::check('article.view-unpublished') AND $article->user_id != Auth::user()->id)
             {
                 abort(403);
             }
