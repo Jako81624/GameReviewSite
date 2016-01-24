1c2d038d73b9fd992dfaafbb6f1bc46165e7477a
Mateusz Ulinski <post159510@gmail.com>
2016-01-24 13:41:34 +0000

    vv

--- app/Http/Controllers/Admin/ImageController.php
+++ app/Http/Controllers/Admin/ImageController.php
@@ -37,13 +37,13 @@ class ImageController extends BackendController
         return $return;
     }
 
-    protected function makeImage($file, $width, $height, $randomFilename, $thumbnail=null)
+    protected function makeImage($file, $height, $width, $randomFilename, $thumbnail=null)
     {
         $md5 = md5_file($file->getRealPath());
         if($thumbnail != null)
-            $img = Image::make($file)->resize($width);
+            $img = Image::make($file)->resize($height);
         else
-            $img = Image::make($file)->fit($width, $height);
+            $img = Image::make($file)->fit($height, $width);
         $path = 'images/';
         if($thumbnail != null)
             $path = 'images/thumb/';
