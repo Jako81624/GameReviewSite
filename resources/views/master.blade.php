b96da65e73b12bd5d3002be8e5af63c35b44b1f7
Jako81624 <jacobwlane@gmail.com>
2016-01-24 12:22:54 +0000

    Designed in MS Paint
    Woot satire

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -15,6 +15,7 @@
     @yield('content')
 <div class="navbar navbar-default navbar-fixed-bottom">
     <p>Open-source and Unlicensed</p>
+    <img src="https://camo.githubusercontent.com/f32e217b2ec23869a55caad8ecb6aee6f5c3b50c/687474703a2f2f666f7274686562616467652e636f6d2f696d616765732f6261646765732f64657369676e65642d696e2d6d732d7061696e742e737667" style="width: 160px;float:left; margin-top:-29px;margin-left: 30px">
     <iframe src="https://ghbtns.com/github-btn.html?user=Jako81624&repo=GameReviewSite&type=fork&count=true" frameborder="0" scrolling="0" width="170px" height="20px" style="float: right; display: inline; margin-top: -27px; margin-right: -60px;"></iframe>
 </div>
     @yield('modals')
