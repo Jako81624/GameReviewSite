7acafa67b2188abf45b903ac7f3e64ef5a6581ed
Bernhard <bernhard@becast.at>
2016-01-24 21:02:43 +0100

    Fixed all games appearing in rows

--- resources/views/pages/allgames.blade.php
+++ resources/views/pages/allgames.blade.php
@@ -8,20 +8,22 @@
 		<h1 id="Home">Games List</h1>
 	</div>
 </div>
-
+<?php $i=1 ?>
 <div id="gamelist">
 <div class="container">
 
 <!-- .................................................... -->
 
-	@foreach($games as $game)
+	@foreach($games->chunk(3) as $game)
 	<div class="row">
+	@foreach ($game as $single)
 		<div class="col-md-4">
 			<div class="thumbnail">
-				<a href="/games/{{$game->slug}}" style="color:inherit"><h4>{{$game->title}}</h4></a>
-				<a href="/games/{{$game->slug}}"><img src="{{$game->temporary_jumbo_image}}"></a>
+				<a href="/games/{{$single->slug}}" style="color:inherit"><h4>{{$single->title}}</h4></a>
+				<a href="/games/{{$single->slug}}"><img src="{{$single->temporary_jumbo_image}}"></a>
 			</div>
 		</div>
+	@endforeach
 	</div>
 	@endforeach
 </div>
