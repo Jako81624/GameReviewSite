2cb7a31bd00bd3ed0582b8b03c8f4b1ea8863ede
Jako81624 <jacobwlane@gmail.com>
2016-01-24 18:27:50 +0000

    PLACEHOLDER

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -14,7 +14,7 @@
             <div class="item ad1 @if($key == 0) active @endif " style="background: url({{$ads->temporary_jumbo_image}}) no-repeat center center fixed; height: 94vh">
                 <div class="carousel-caption">
                 <a href="/games/{{$ads->slug}}" style="color:inherit"><h1>{{$ads->title}}</h1></a>
-                <!--<p>{!!$ads->intro!!}</p>-->
+                <p><!--{!!$ads->intro!!}-->TEXT THIS LONG NOT MORE I'M STALLING</p>
                 <!--<img src="{{$ads->temporary_jumbo_image}}" alt="...">-->
                 </div>
             </div>
