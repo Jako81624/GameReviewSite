fb86b3986ba0019bacd74957ec46fea664a9d3d4
Mateusz Ulinski <post159510@gmail.com>
2016-01-24 13:44:06 +0000

    wtf why is this library doing that

--- app/Http/Controllers/Admin/ImageController.php
+++ app/Http/Controllers/Admin/ImageController.php
@@ -41,9 +41,9 @@ class ImageController extends BackendController
     {
         $md5 = md5_file($file->getRealPath());
         if($thumbnail != null)
-            $img = Image::make($file)->fit  ($height);
+            $img = Image::make($file)->resize($height);
         else
-            $img = Image::make($file)->fit($width, $height);
+            $img = Image::make($file)->resize($width, $height);
         $path = 'images/';
         if($thumbnail != null)
             $path = 'images/thumb/';
