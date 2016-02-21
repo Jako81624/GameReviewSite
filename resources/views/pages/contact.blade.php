6d012e7ad6ad679f967790e9dbd0e126240bfc18
Jako81624 <jacobwlane@gmail.com>
2016-02-21 19:52:31 +0000

    Cleaning up old email addresses

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -21,10 +21,6 @@
 </style>
 
 <div class="container">
-	<h3>General Enquiries</h3>
-	<p>For general enquiries you can contact us at: enquiries@ocgr.io</p>
-	<h3>Writing Enquiries</h3>
-	<p>For writing enquiries you can contact us at: writing@ocgr.io</p>
 	<h3>Security Enquiries</h3>
 	<p>For general issues, security flaws or potential exploits please contact the admin at admin@ocgr.io, or preferably Posttwo who can be reached at posttwo@posttwo.pt.</p>
 	<h3>Business Enquiries</h3>
