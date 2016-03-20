6c385ca508eff952604abe21f6b351bceb09e477
Posttwo <posttwo@users.noreply.github.com>
2016-03-20 13:26:02 +0000

    Create 2016_03_21_202508_interviews.php

---
+++ database/migrations/2016_03_21_202508_interviews.php
@@ -0,0 +1,27 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class TldrFieldForArticles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function ($table) {
+            $table->boolean('interview');
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function ($table) {
+            $table->dropColumn('interview');
+        });
+    }
