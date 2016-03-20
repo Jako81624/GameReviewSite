a02099f8e86b1b0ddb01c79174543940682545ff
Posttwo <posttwo@users.noreply.github.com>
2016-03-20 13:27:08 +0000

    Update 2016_03_21_202508_interviews.php

--- database/migrations/2016_03_21_202508_interviews.php
+++ database/migrations/2016_03_21_202508_interviews.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-class TldrFieldForArticles extends Migration
+class Interviews extends Migration
 {
     /**
      * Run the migrations.
